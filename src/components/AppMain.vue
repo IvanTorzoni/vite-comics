f74490974ca21b7dd943d41fd36568464efbffda
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-05-02 20:30:11 +0200

    feat: added static images in section images of the main component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,6 +11,30 @@ export default {
             <h2> --> Content goes here <-- </h2>
         </div>
     </section>
+    <section class="images">
+        <div class="container">
+            <div class="options">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="Digital Comic">
+                <span>DIGITAL COMICS</span>
+            </div>
+            <div class="options">
+                <img src="../assets/img/buy-comics-merchandise.png" alt="Merch">
+                <span>DC MERCHANDISE</span>
+            </div>
+            <div class="options">
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="Subscription">
+            <span>SUBSCRIPTION</span>                
+            </div>
+            <div class="options">
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="Closest Store">
+            <span>COMIC SHOP LOCATOR</span>                
+            </div>
+            <div class="options">
+                <img class="pay" src="../assets/img/buy-dc-power-visa.svg" alt="Payment methods">
+            <span>DC POWER VISA</span>                
+            </div>
+        </div>
+    </section>
 </template>
 
 <style scoped lang="scss">
@@ -29,5 +53,33 @@ export default {
     .container {
         @include container(space-between, 80%, 0);
     }
-}    
+}
+
+.images {
+    display: flex;
+    width: 100%;
+    background-color: $bg-color-images;
+    color: $color-images;
+    height: 200px;
+
+    img{
+        max-width: 50%;
+        padding-right: 30px;
+    }
+
+    .pay{
+        width: 40%
+    }
+
+    .options{
+        display: flex;
+        align-items: center
+    }
+
+    .container {
+        @include container(space-between, 80%, 0);
+        align-items: center;
+    }
+}
+
 </style>
