3a1d89605b639cd72bb6339c1455d10a733be4c1
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-05-05 16:00:24 +0200

    feat: added props for comics display in main component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,33 +1,64 @@
 <script>
-export default {
+import { comicsArray } from "../assets/data/comic.js";
+import ComicDisplayer from "./ComicDisplayer.vue";
 
-}
+
+export default {
+    components: {
+        ComicDisplayer,
+    },
+    data() {
+        return {
+            comic: comicsArray,
+        };
+    },
+};
 
 </script>
 
 <template>
     <section class="content">
         <div class="container">
-            <h2> --> Content goes here <-- </h2>
+            <div class="row">
+                <div class="col" v-for="comicDisplay in comic">
+                    <ComicDisplayer 
+                    :comicImage="comicDisplay.thumb" 
+                    :comicSeries="comicDisplay.series" 
+                    />
+                </div>
+            </div>
         </div>
     </section>
 </template>
 
 <style scoped lang="scss">
-    @use "../style/partials/mixin" as *;
-    @use "../style/partials/variables" as *;
+@use "../style/partials/mixin" as *;
+@use "../style/partials/variables" as *;
 
-.content{
+.content {
     display: flex;
     align-items: center;
+    height: 800px;
     width: 100%;
     margin-top: 30px;
     background-color: $bg-color-content;
     color: $color-content;
-    height: 200px;
 
     .container {
         @include container(space-between, 80%, 0);
-    }
-}
-</style>
+
+
+        .row {
+            display: flex;
+            flex-direction: row;
+            align-items: stretch;
+            flex-wrap: wrap;
+        }
+        .col {
+            width: 15%;
+            margin-bottom: 1rem;
+            color: white;
+        }
+
+        }
+    }</style>
---
+++ src/components/ComicDisplayer.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    comicImage: String,
+    comicSeries: String,
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <img :src="comicImage" alt="" />
+    <h4>{{ comicSeries }}</h4>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  padding: 1rem;
+  text-align: center;
+
+  img {
+    max-width: 100%;
+  }
+}
+</style>
